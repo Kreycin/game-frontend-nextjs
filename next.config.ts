730057867efb5b3a0ee8e57a42968d1bf8bb2b0a
Nick <nick@MacBookAirNick.local>
2025-09-17 11:57:59 +0700

    แคช

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,19 @@
 import type { NextConfig } from "next";
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  sw: 'service-worker.js',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === 'development',
+  // --- การแก้ไข ---
+  // ย้าย importScripts เข้ามาไว้ใน workboxOptions
+  workboxOptions: {
+    importScripts: ['/firebase-messaging-sw.js'],
+  },
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
