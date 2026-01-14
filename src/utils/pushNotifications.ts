801cb2ae8ca17cc9591d37515b471e6fd678e47e
Kreycin <kreycin@Kreycins-MacBook-Air.local>
2026-01-14 11:26:54 +0700

    fix: TypeScript error - cast Uint8Array.buffer to ArrayBuffer

--- src/utils/pushNotifications.ts
+++ src/utils/pushNotifications.ts
@@ -69,7 +69,7 @@ export async function subscribeToPush(): Promise<boolean> {
         // Subscribe to push
         const subscription = await registration.pushManager.subscribe({
             userVisibleOnly: true,
-            applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+            applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY).buffer as ArrayBuffer,
         });
 
         // Send subscription to backend
